11a82fdb80c86b635d78425ad0039c588b8c8cdc
ibrahimohid <ibrahimohid77@gmail.com>
2025-08-13 23:21:47 +0600

    hearder,sidebar&movieList added

---
+++ src/Sidebar.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import Trending from './assets/icons/trending.svg';
+import NewRelease from './assets/icons/newRelease.svg';
+import ComingSoon from './assets/icons/comingSoon.svg';
+import Favorites from './assets/icons/favorite.svg';
+import WatchLater from './assets/icons/watchLater.svg'
+
+
+const Sidebar = () => {
+  return (
+    <>
+      {/* Sidebar */}
+      <aside>
+        <ul className="space-y-2">
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg bg-[#00D991] text-black"
+              href="#"
+            >
+              <img
+                src={Trending}
+                width={24}
+                height={24}
+                alt="trending"
+              />
+              <span>Trending</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img
+                src={NewRelease}
+                width={24}
+                height={24}
+                alt="newRelease"
+              />
+              <span>New Releases</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img
+                src={ComingSoon}
+                width={24}
+                height={24}
+                alt="comingSoon"
+              />
+              <span>Coming Soon</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img
+                src={Favorites}
+                width={24}
+                height={24}
+                alt=""
+              />
+              <span>Favorites</span>
+            </a>
+          </li>
+          <li>
+            <a
+              className="flex items-center space-x-2 px-5 py-3.5 rounded-lg"
+              href="#"
+            >
+              <img
+                src={WatchLater}
+                width={24}
+                height={24}
+                alt=""
+              />
+              <span>Watch Later</span>
+            </a>
+          </li>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
